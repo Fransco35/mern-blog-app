b8fbfdc81bec6bb064be84a2632e78955fb71332
Francis Inegbenose <62514538+Fransco35@users.noreply.github.com>
2023-07-16 16:28:48 +0100

    Updated setupProxy.js target

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,7 +3,7 @@ module.exports = (app) => {
   app.use(
     ["/api*"],
     createProxyMiddleware({
-      target: "http://localhost:3001",
+      target: "https://rise-blog-backend.onrender.com",
     })
   );
 };
