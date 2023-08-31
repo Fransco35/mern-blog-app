def0d1e573946399a9ff586a2a06bbe446b29803
Francis Inegbenose <kizzycole10@gmail.com>
2023-08-31 12:13:17 +0100

    update route

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const cloudinary = require("./utils/cloudinary");
 const upload = require("./utils/multer");
 
 const corsOptions = {
-  origin: "https://riseblog.onrender.com",
+  origin: "https://thoughtful-worm-tweed-jacket.cyclic.app",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   optionsSuccessStatus: 200,
   credentials: true,
